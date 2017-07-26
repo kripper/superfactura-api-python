86c2530d079e08f9eec723f1f2f53aa45e2fda61
Christopher Pereira <kripper@imatronix.com>
2017-07-26 17:22:10 +0200

    Initial release

---
+++ SuperFacturaAPI/SuperFacturaAPI.py
@@ -0,0 +1,79 @@
+import httplib, urllib
+import zlib
+import json
+import base64
+
+class SuperFacturaAPI:
+	version = '1.4-ruby'
+	user = ''
+	password = ''
+
+	def __init__(self, user, password):
+		self.user = user
+		self.password = password
+
+	def SendDTE(self, data, ambiente, options):
+		options['ambiente'] = ambiente
+		options['encoding'] = 'UTF-8'
+		options['version'] = self.version
+
+		if options['savePDF']:
+			options['getPDF'] = 1
+
+		if options['saveXML']:
+			options['getXML'] = 1
+
+		output = self.SendRequest(data, options)
+		obj = json.loads(output)
+		if obj['ack'] != 'ok':
+			text = obj['response']['title'] + " - " if obj['response']['title'] != "" else "" + obj['response']['message']
+			raise ValueError("ERROR: " + text)
+
+		appRes = obj['response']
+
+		folio = appRes['folio']
+		if appRes['ok'] == "1":
+			savePDF = options['savePDF']
+			if savePDF:
+				self.WriteFile(savePDF + ".pdf", self.DecodeBase64(appRes['pdf']));
+
+				if appRes['pdfCedible']:
+					self.WriteFile(savePDF + "-cedible.pdf", self.DecodeBase64(appRes['pdfCedible']));
+
+			saveXML = options['saveXML']
+			if saveXML:
+				self.WriteFile(saveXML + ".xml", appRes['xml'].encode('latin-1'));
+		else:
+			raise ValueError(output)
+
+		return appRes
+
+	def SendRequest(self, data, options):
+		params = urllib.urlencode({
+			'user': self.user,
+			'pass': self.password,
+			'content': json.dumps(data, encoding='latin1'),
+			'options': json.dumps(options, encoding='latin1')
+		})
+
+		headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+		conn = httplib.HTTPConnection("superfactura.cl:80")
+		conn.request("POST", "?a=json", params, headers)
+		response = conn.getresponse()
+		body = response.read();
+		conn.close()
+		return self.Decompress(body)
+
+	def Decompress(self, gzip):
+		return zlib.decompress(gzip, 16+zlib.MAX_WBITS)
+
+	def DecodeBase64(self, b64):
+		return b64.decode('base64')
+		# return base64.decodebytes(b64)
+		# return base64.standard_b64decode(b64)
+		#return binascii.a2b_base64(b64)
+
+	def WriteFile(self, filename, content):
+		f = open(filename, "wb")
+		f.write(content)
+		f.close()
---
+++ ejemplo-2.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: ISO-8859-1 -*-
+
+# Ejemplo similar al ejemplo-1, pero acá se obtienen además el PDF, el PDF-Cedible y el XML desde SuperFactura.
+
+from SuperFacturaAPI import SuperFacturaAPI
+
+# 1) Generar arreglo con datos del DTE
+
+datos = {
+	'Encabezado': {
+		'IdDoc': {
+			'TipoDTE': 33,
+			# 'FchEmis': '2015-01-01', # Opcional
+		},
+		'Emisor': {
+			'RUTEmisor': '99581150-2',
+			# Los demás datos serán agregados por SuperFactura
+		},
+		'Receptor': {
+			'RUTRecep': '1-9',
+			'RznSocRecep': 'Test',
+			'GiroRecep': 'Giro',
+			'DirRecep': 'Dirección',
+			'CmnaRecep': 'Comuna',
+			'CiudadRecep': 'Ciudad',
+		},
+		# 'Totales' será agregado por SuperFactura
+	},
+	'Detalles': [
+		{
+			# 'NroLinDet' será agregado por SuperFactura
+			'NmbItem': 'Item 1',
+			'DscItem': 'Descripción del item 1',
+			'QtyItem': 3,
+			'UnmdItem': 'KG',
+			'PrcItem': 100
+		},
+		{
+			'NmbItem': 'Item 2',
+			'DscItem': 'Descripción del item 2',
+			'QtyItem': 5,
+			'UnmdItem': 'KG',
+			'PrcItem': 65
+		}
+	]
+}
+
+# 2) Usar API para generar y enviar el DTE al SII
+
+api = SuperFacturaAPI.SuperFacturaAPI('usuario@cliente.cl', 'mypassword')
+
+resultado = api.SendDTE(
+	datos,								# Datos del DTE
+	'cer',								# Ambiente: 'pro' = producción y 'cer' = certificación
+	{									# El tercer argumento puede contener un arreglo con opciones
+		'savePDF': '/tmp/dte-123',		# Descarga y almacena el PDF
+		'saveXML': '/tmp/dte-123'		# Descarga y almacena el XML
+	}
+)
+
+# 3) Procesar salida de la API
+
+if resultado['ok']:
+	print 'Folio: ', resultado['folio']
+else:
+	print 'Error'
